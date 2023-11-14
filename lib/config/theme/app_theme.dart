3e8394f5c8cdd479cbc2847cf91c50109205d819
Santicm23 <santicastrom@outlook.com>
2023-11-14 07:06:51 -0500

    Update app theme and main.dart

--- lib/config/theme/app_theme.dart
+++ lib/config/theme/app_theme.dart
@@ -1,7 +1,12 @@
 import 'package:flutter/material.dart';
 
 class AppTheme {
-  get theme => ThemeData(
-    
-  );
+  get themeData => ThemeData(
+        useMaterial3: true,
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        appBarTheme: const AppBarTheme(
+          centerTitle: true,
+        ),
+        brightness: Brightness.dark,
+      );
 }
