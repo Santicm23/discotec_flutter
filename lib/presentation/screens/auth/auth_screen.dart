5b4424621b2e0c83005d98c71d8a2393ac8e9b2b
Santicm23 <santicastrom@outlook.com>
2023-11-16 08:36:21 -0500

    Add permissions and fingerprint authentication, update MainActivity and AuthScreen, and add local_auth plugin for biometric auth

--- lib/presentation/screens/auth/auth_screen.dart
+++ lib/presentation/screens/auth/auth_screen.dart
@@ -1,6 +1,8 @@
 import 'package:discotec_flutter/presentation/screens/auth/widgets/auth_card_widget.dart';
 import 'package:discotec_flutter/presentation/screens/auth/widgets/custom_title_widget.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:local_auth/local_auth.dart';
 
 class AuthScreen extends StatelessWidget {
   static const routeName = '/auth';
@@ -9,17 +11,19 @@ class AuthScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const _AuthView();
+    return _AuthView();
   }
 }
 
 class _AuthView extends StatelessWidget {
-  const _AuthView();
+  final LocalAuthentication _auth = LocalAuthentication();
+
+  _AuthView();
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
+    return Scaffold(
+      body: const Center(
         child: SingleChildScrollView(
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
@@ -30,6 +34,30 @@ class _AuthView extends StatelessWidget {
             ],
           ),
         ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () async {
+          try {
+            final bool didAuthenticate = await _auth.authenticate(
+                localizedReason: 'Please authenticate to show account balance',
+                options: const AuthenticationOptions(useErrorDialogs: false));
+          } on PlatformException catch (e) {
+            // ignore: use_build_context_synchronously
+            showDialog(context: context, builder: (context) {
+              return AlertDialog(
+                title: const Text('Error'),
+                content: Text(e.message ?? ''),
+                actions: [
+                  TextButton(
+                    onPressed: () => Navigator.of(context).pop(),
+                    child: const Text('Ok'),
+                  ),
+                ],
+              );
+            },);
+          }
+        },
+        child: const Icon(Icons.fingerprint),
       ),
     );
   }
